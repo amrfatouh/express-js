8e809ea352989844bf72bc062c8dd62ea173b8ba
amrfatouh <66204477+amrfatouh@users.noreply.github.com>
2020-11-06 02:35:35 +0200

    serving html pages

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -8,7 +10,11 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRouter);
+app.use("/admin", adminRouter);
 app.use(shopRouter);
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "views", "404.html"));
+});
 
 app.listen(8000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,13 @@
+const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.send("<h1>Hello from Express</h1>");
+  console.log("dirname: ", __dirname);
+  console.log('dirname + ".." + "..": ', path.join(__dirname, "..", ".."));
+  res.sendFile(path.join(__dirname, "..", "views", "shop.html"));
 });
 
 module.exports = router;
